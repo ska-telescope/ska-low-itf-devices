17848043017f8e4e71dac8fc75e0ff32303a64de
Geoffrey Mant <geoffrey.mant@stfc.ac.uk>
2024-09-19 12:32:20 +0100

    MCCS-2267 add non-sequential indexed support for z16

--- src/ska_snmp_device/definitions.py
+++ src/ska_snmp_device/definitions.py
@@ -14,7 +14,11 @@ from pysnmp.smi.builder import MibBuilder
 from pysnmp.smi.compiler import addMibCompiler
 from tango import AttrWriteType
 
-from ska_snmp_device.types import SNMPAttrInfo, attr_args_from_snmp_type
+from ska_snmp_device.types import (
+    SNMPAttrInfo,
+    attr_args_from_snmp_type,
+    dtype_string_to_type,
+)
 
 
 @contextmanager
@@ -55,7 +59,6 @@ def _build_attr_info(mib_builder: MibBuilder, attr: dict[str, Any]) -> SNMPAttrI
     useful metadata about the attribute in an SNMPAttrInfo, including
     suitable arguments to pass to tango.server.attribute().
     """
-
     # Pop off the values we're going to use in this function. The rest will
     # be used as overrides to the generated tango.server.attribute() args.
     mib_name, symbol_name, *_ = oid = tuple(attr.pop("oid"))
@@ -64,12 +67,25 @@ def _build_attr_info(mib_builder: MibBuilder, attr: dict[str, Any]) -> SNMPAttrI
     # get metadata about the SNMP object definition in the MIB
     (mib_info,) = mib_builder.importSymbols(mib_name, symbol_name)
 
+    if isinstance(attr.get("dtype"), str):
+        try:
+            attr["dtype"] = dtype_string_to_type(attr["dtype"])
+        except KeyError as exc:
+            raise TypeError(
+                f"The string type \"{attr['dtype']}\" "
+                f"provided for attribute \"{attr['name']}\" "
+                "has no associated Python type"
+            ) from exc
+
     # Build args to be passed to tango.server.attribute()
     attr_args = {
         "access": {
             "readonly": AttrWriteType.READ,
+            "read-only": AttrWriteType.READ,
             "writeonly": AttrWriteType.WRITE,
+            "write-only": AttrWriteType.WRITE,
             "readwrite": AttrWriteType.READ_WRITE,
+            "read-write": AttrWriteType.READ_WRITE,
         }[mib_info.maxAccess],
         **attr_args_from_snmp_type(mib_info.syntax),
         **attr,  # allow user to override generated args
@@ -111,10 +127,20 @@ def _expand_attribute(attr: Any) -> Generator[Any, None, None]:
     of the cartesian product of the provided ranges. The "name" field
     of each attribute will be formatted with name.format(i1, ..., iN).
 
+    Additionally "indexes" may have an increment to allow for equally spaced
+    non sequential oid's [a, b, c] where b = a + (N-1)c. In this case it may also
+    be desireable for the name to run sequentially. Therefore 2 additional specifiers
+    have been added "start_index" and  "placeholder_index" from which the
+    names formating placeholder_index is substituted which the new starting index
+    (i1 ..., placeholder_index, ... iN)
+
     If "indexes" is not present, attr will be yielded unmodified. This
     function also performs some validation on the attribute definition.
     """
+    start_index = None
     suffix = attr["oid"][2:]
+    start_index = attr.pop("start_index", None)
+    placeholder_index = attr.pop("placeholder_index", 0)
     indexes = attr.pop("indexes", [])
     name = attr["name"]
 
@@ -135,14 +161,22 @@ def _expand_attribute(attr: Any) -> Generator[Any, None, None]:
                 f'OID for attribute "{name}" must have a suffix - use 0 for a scalar object'
             )
 
-    index_ranges = (range(a, b + 1) for a, b in indexes)
+    index_ranges = (
+        (range(v[0], v[1] + v[2], v[2])) if len(v) > 2 else (range(v[0], v[1] + 1))
+        for v in indexes
+    )
     for element in itertools.product(*([i] for i in suffix), *index_ranges):
         index_vars = element[len(suffix) :]  # black wants this space T_T
-        formatted_name = name.format(*index_vars)
+        index_vars_list = list(index_vars)
+        if start_index is not None:
+            index_vars_list[placeholder_index] = start_index
+            start_index += 1
+        formatted_name = name.format(*index_vars_list)
         if not formatted_name.isidentifier():
             raise ValueError(
                 f'Attribute name "{formatted_name}" is not a valid Python identifier'
             )
+
         yield {
             **attr,
             "name": formatted_name,
