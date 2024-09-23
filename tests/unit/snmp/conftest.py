bddc0afe42cc5d6621167deaafd3f77b174e20b9
Geoffrey Mant <geoffrey.mant@stfc.ac.uk>
2024-09-23 13:39:14 +0100

    MCCS-2267 add debug

--- tests/unit/snmp/conftest.py
+++ tests/unit/snmp/conftest.py
@@ -99,6 +99,7 @@ def simulator():
         try:
             while sim_process.poll() is None:
                 line = sim_process.stderr.readline()
+                logging.debug(f"Simulator readline output: {line}")
                 if line.startswith(f"  Listening at UDP/IPv4 endpoint {host}:{port}"):
                     yield host, port
                     break
